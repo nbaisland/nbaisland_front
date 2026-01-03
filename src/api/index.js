daf29d0442427ab0c16bdf3700c60ab542e5cca2
Nate Klassen <nateklassen34@gmail.com>
2026-01-03 14:04:11 -0500

    Api url shenaningans

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
-const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:8080/api';
+const rawBase = import.meta.env.VITE_API_URL ?? 'http://localhost:8080';
+const API_BASE = rawBase.replace(/\/$/, '') + '/api';
 
 function getAuthHeaders() {
   const token = localStorage.getItem('token');
